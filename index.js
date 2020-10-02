9083a1e8a8cb5218aa56289021bbfdf0dddbd3bb
taibislamdipu <mailtaibislam@gmail.com>
2020-10-03 04:19:34 +0600

    server connection done

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.is4kq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const eventCollection = client.db("volunteerNetworkDB").collection("events");
+    console.log('Database connected successfully!!');
+
+    // create event
+    app.post("/addEvent", (req, res) => {
+        const event = req.body;
+        eventCollection.insertOne(event)
+            .then(result => {
+                console.log(result);
+            })
+    })
+});
+
+
+
+app.get('/', (req, res) => {
+    res.send('Volunteer network server is ready!')
+})
+
+app.listen(port)
